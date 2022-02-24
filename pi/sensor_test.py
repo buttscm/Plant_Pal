ee06519310db0b1033cf5bea84d5c06a73a01174
Kevin Eaton <eatonko@mail.uc.edu>
2022-02-24 16:38:26 -0500

    Added working sensor testing

--- pi/sensor_test.py
+++ pi/sensor_test.py
@@ -3,27 +3,49 @@
 # This will be used to test the functionality of our hardware sensors
 # that will go into the plant.
 
-import RPi.GPIO as GPIO
+import busio
+import digitalio
+import board
+import adafruit_mcp3xxx.mcp3008 as MCP
 import time
+import RPi.GPIO as GPIO
+from adafruit_mcp3xxx.analog_in import AnalogIn
 
 
-def TestSunlight():
-    GPIO.setmode(GPIO.BCM)
+
+def TestAnalogInputs():
+    #Setup for SPI analog to digital converter
+    spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+    cs = digitalio.DigitalInOut(board.D5)
+    mcp = MCP.MCP3008(spi, cs)
+    #P0 first channel, P1 next, so on
+    sunlight = AnalogIn(mcp, MCP.P0)
+    moisture = AnalogIn(mcp, MCP.P1)
     
-    #setup the GPIO for input on the pin i have it plugged into
-    GPIO.setup(21, GPIO.IN)
+    #Setup GPIO
+    #GPIO.setmode(GPIO.BCM)
+    #GPIO.setup(DataPin,  GPIO.OUT)
     
-    print("Sunlight test")
+    #Infinite loop read voltage
+    #Good way to check if there are readings in
+    print("Testing Sunlight and Moisture Sensor values (ctrl c to stop)")
+    time.sleep(3)
+    
+    while True:  
+        print('Raw Sunlight ADC Value (P0): ', sunlight.value)
+        print('Sunlight Voltage: ' + str(round(sunlight.voltage, 2)) + 'V\n')
+        print('Raw Moisture ADC Value (P1): ', moisture.value)
+        print('Moisture Voltage: ' + str(round(moisture.voltage, 2)) + 'V\n\n')
+        #if channel.voltage > 2.0:
+            #GPIO.output(DataPin, True)
+        #else:
+            #GPIO.output(DataPin, False)
+        
+        time.sleep(1)
 
-    #constantly read, see what the output is
-    #works, but we want analog output
-    while True:
-        print(float(GPIO.input(21)))
 
-def TestPump():
-    print("test pump")
 def main():    
-    TestPump()
+    TestAnalogInputs()
 
 if(__name__ == "__main__"):
     main()
